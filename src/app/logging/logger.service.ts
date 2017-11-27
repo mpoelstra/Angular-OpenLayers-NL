6cd48258697c8af2d7a7c27bd34ec9f06cd61369
Mark Poelstra <mark.poelstra@clockwork.nl>
2017-11-27 16:10:50 +0100

    fixes logging

--- src/app/logging/logger.service.ts
+++ src/app/logging/logger.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
@@ -9,19 +9,23 @@ export class LoggerService {
 
   log(type, text): Observable<Object> {
 
+    // Initialize Params Object
+    let Params = new HttpParams();
+
+    Params = Params.append('text', text);
+    Params = Params.append('app', 'PBM');
+    Params = Params.append('browser', window.navigator.userAgent);
+    Params = Params.append('userId', 'pbm123');
+
     const url = 'http://192.168.56.104:8080/geoserver/rest/log/logInfo';
-    const body = JSON.stringify({
-      text: text,
-      app: 'PBM',
-      browser: window.navigator.userAgent,
-      userId: 'pbm123'
-    });
+    const body = Params.toString();
  
     let headers = new HttpHeaders();
-    headers = headers.append("Authorization", "Basic " + btoa("admin:geoserver"));
-    headers = headers.append("Content-Type", "application/x-www-form-urlencoded");
+    //headers = headers.append("Authorization", "Basic " + btoa("admin:geoserver"));
+    //headers = headers.append("Content-Type", "application/x-www-form-urlencoded");
  
-    return this.http.post(url,body, {headers: headers});
+    //return this.http.post(url,body, {headers: headers});
+    return this.http.post(url,body);
     /*
     .subscribe(response => {
           console.log(response);
